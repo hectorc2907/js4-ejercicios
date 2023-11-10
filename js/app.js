2422fa9737134813c4ee32915cf2344b6aae0d4d
Hector Alberto Clessi <hectorc2907@gmail.com>
2023-11-09 22:50:35 -0300

    se realiza el maquetado del ejercicio 2, falta agregar la parte logica

--- js/app.js
+++ js/app.js
@@ -38,3 +38,47 @@ function ejercicio1() {
 }
 let punto1 = document.getElementById("btnPunto1");
 punto1.addEventListener("click", ejercicio1);
+
+function ejercicio2() {
+  let mensajePunto2 = document.getElementById("mensajePunto2");
+  let mensajeEstadoCuenta = document.getElementById("mensajeEstadoCuenta");
+  let cuenta = {
+    titular: "",
+    saldo: 0,
+    ingresarSaldo() {
+      this.saldo += parseInt(prompt("Ingresar Saldo: "));
+      this.informarSaldo();
+    },
+    extraerSaldo() {
+      this.saldo -= parseInt(prompt("Extraer Saldo: "));
+      this.informarSaldo();
+    },
+    informarSaldo() {
+      if (this.saldo === 0) {
+        mensajeEstadoCuenta.textContent = `La cuenta se encuentra en 0`;
+      } else if (this.saldo > 0) {
+        mensajeEstadoCuenta.textContent = `La cuenta es positiva con $${this.saldo}`;
+      } else {
+        mensajeEstadoCuenta.textContent = `La cuenta es negativa con -$${this.saldo}`;
+      }
+    },
+  };
+  cuenta.titular =
+    prompt("Ingrese el Nombre del titular de la cuenta: ") || "Desconocido";
+  mensajePunto2.innerHTML = `Titular: ${cuenta.titular}<br>Saldo: ${cuenta.saldo}<br>`;
+  let btnIngresar = document.getElementById("btnIngresarDinero");
+  btnIngresar.addEventListener("click", function () {
+    cuenta.ingresarSaldo();
+  });
+  let btnInforme = document.getElementById("btnInforme");
+  btnInforme.addEventListener("click", function () {
+    cuenta.informarSaldo();
+  });
+  let btnExtraerDinero = document.getElementById("btnExtraerDinero");
+  btnExtraerDinero.addEventListener("click", function () {
+    cuenta.extraerSaldo();
+  });
+  mensajePunto2.innerHTML = `Titular: ${cuenta.titular}<br>Saldo: ${cuenta.saldo}<br>`;
+}
+let punto2 = document.getElementById("btnPunto2");
+punto2.addEventListener("click", ejercicio2);
